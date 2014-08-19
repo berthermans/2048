ef3c877143d5d12ec5449a6622640d3802a2a330
Bert Hermans <bert.hermans@capgemini.com>
2014-08-19 14:41:25 +0200

    Move and compare
    Programmed up and left
    Added move function
    Added compare function
    Hiding 0-value blocks

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -8,6 +8,7 @@ app.controller('puzzleController', function ($scope) {
 
 	settings.blockWidth = 100 / settings.gridSize;
 	settings.blockHeight = 100 / settings.gridSize;
+	settings.blockCount = Math.pow(settings.gridSize, 2);
 
 	// make settings public to the scope
 	angular.extend($scope, settings);
@@ -26,7 +27,7 @@ app.controller('puzzleController', function ($scope) {
 			var block = {
 
 				// create random value for top 4 blocks
-				val: (row < 2 && col < 2) ? rand(2) : 0,
+				val: (row < 4 && col < 4) ? rand(2) : 0,
 				row: row,
 				col: col
 			};
@@ -35,10 +36,56 @@ app.controller('puzzleController', function ($scope) {
 		}
 	}
 
-	$scope.left = function(){	$scope.operand = "left"; }
-	$scope.right = function(){	$scope.operand = "right"; }
-	$scope.up = function(){		$scope.operand = "up"; }
-	$scope.down = function(){	$scope.operand = "down"; }
+	$scope.move = function(dir){
+		$scope.operand = dir;
+
+		// default loop
+		if(dir == "up" || dir == "left"){
+
+			angular.forEach($scope.grid, function(block, key){
+
+				// Only process blocks with value
+				if(block.val){
+
+					// Only loop through 2nd row and higher
+					if(dir == "up" && block.row > 0){
+						var compareBlock = $scope.grid[(block.row - 1) + ":" + block.col];
+						
+						compare(block, compareBlock);
+					}
+					// Only loop through 2nd column and higher
+					else if(dir == "left" && block.col > 0){
+						var compareBlock = $scope.grid[block.row + ":" + (block.col - 1)];
+						
+						compare(block, compareBlock);
+					}
+				}
+			});
+		}
+	}
+
+	function compare(block, compareBlock){
+
+		// Check if current block value is equal to prevBlock value
+		if(block.val == compareBlock.val){
+
+			// Clear current block
+			block.val = 0;
+
+			// Multiply prevBlock value by 2
+			compareBlock.val *= 2;
+		}
+
+		// In case compareBlock is empty
+		else if(compareBlock.val == 0){
+
+			// Update empty block with current block value
+			compareBlock.val = block.val;
+
+			// Clear current block
+			block.val = 0;
+		}
+	}
 });
 
 // Create random number between 2 and 2^(pow)
